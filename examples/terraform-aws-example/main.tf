7d22e1da6b94ed1e8da5135dad7759f71e9f69a4
Taiki Fnit Watanabe <taiki.watanabe@mixi.co.jp>
2023-08-29 13:20:07 +0900

    add aws example

---
+++ examples/terraform-aws-example/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 0.12.26"
+}
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0d5d9d301c853a04a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<EOF
+#!/bin/bash
+echo "Hellow, FnitFnit!" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
+}
+
+resource "aws_security_group" "instance" {
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
