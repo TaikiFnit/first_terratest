f9e6bae9b28b3886e2ba34a9c17b0f001ef0a4fe
Taiki Fnit Watanabe <taiki.watanabe@mixi.co.jp>
2023-08-29 13:12:06 +0900

    hello world

---
+++ test/terraform_hello_world_example_test.go
@@ -0,0 +1,18 @@
+package test
+import (
+  "testing"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+  "github.com/stretchr/testify/assert"
+)
+func TestTerraformHelloWorldExample(t *testing.T) {
+  terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+    TerraformDir: "../examples/terraform-hello-world-example",
+  })
+  defer terraform.Destroy(t, terraformOptions)
+  terraform.InitAndApply(t, terraformOptions)
+  output := terraform.Output(t, terraformOptions, "hello_world")
+  assert.Equal(t, "Hello, FnitFnit!", output)
+  assert.Equal(t, "Hello, FnitFnit!", output)
+  assert.Equal(t, "Hello, FnitFnit!", output)
+  assert.Equal(t, "Hello, FnitFnit!", output)
+}
