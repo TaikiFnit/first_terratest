ccefc55e429ae642c0781fef8973f839c8ae53c4
TanukiFnit <taikifnitspg@gmail.com>
2023-09-12 05:00:49 +0000

    fix sytax errors

--- test/terraform_gcp_example_test.go
+++ test/terraform_gcp_example_test.go
@@ -1,76 +1,76 @@
 package test
-import (
-  "fmt"
-  "strings"
-  "testing"
-  "time"
 
-  "github.com/gruntwork-io/terratest/modules/gcp"
-  "github.com/gruntwork-io/terratest/modules/random"
-  "github.com/gruntwork-io/terratest/modules/retry"
-  "github.com/gruntwork-io/terratest/modules/ssh"
-  "github.com/gruntwork-io/terratest/modules/terraform"
-  test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
-  "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/gcp"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/gruntwork-io/terratest/modules/ssh"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformGcpExample(t *testing.T) {
-  t.Parallel()
+	t.Parallel()
 
-  exampleDir := test_structure.CopyTerraformFolderToTemp(t, "../../", "examples/terraform-gcp-example")
-  projectId := gcp.GetGoogleProjectIDFromEnvVar(t)
-  zone := "us-east1-b"
+	exampleDir := test_structure.CopyTerraformFolderToTemp(t, "../../", "examples/terraform-gcp-example")
+	projectId := gcp.GetGoogleProjectIDFromEnvVar(t)
+	zone := "us-east1-b"
 
-  expectedBucketName := fmt.Sprintf("terratest-gcp-example-%s", strings.ToLower(random.UniqueId())))
-  expectedInstanceName := fmt.Sprintf("terratest-gcp-example-%s", strings.ToLower(random.UniqueId()))
+	expectedBucketName := fmt.Sprintf("terratest-gcp-example-%s", strings.ToLower(random.UniqueId()))
+	expectedInstanceName := fmt.Sprintf("terratest-gcp-example-%s", strings.ToLower(random.UniqueId()))
 
-  terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-    TerraformDir: exampleDir,
-    Vars: map[string]interface{}{
-      "gcp_project_id": projectId,
-      "zone": zone,
-      "instance_name": expectedInstanceName,
-      "bucket_name": expectedBucketName,
-    },
-  })
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: exampleDir,
+		Vars: map[string]interface{}{
+			"gcp_project_id": projectId,
+			"zone":           zone,
+			"instance_name":  expectedInstanceName,
+			"bucket_name":    expectedBucketName,
+		},
+	})
 
-  defer terraform.Destroy(t, terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
 
-  terraform.InitAndApply(t terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 
-  bucketURL := terraform.Output(t, terraformOptions, "bucket_url")
-  instanceName := terraform.Output(t, terraformOptions, "instance_name")
+	bucketURL := terraform.Output(t, terraformOptions, "bucket_url")
+	instanceName := terraform.Output(t, terraformOptions, "instance_name")
 
-  expectedURL := fmt.Sprintf("gs://%s", expectedBucketName)
-  assert.Equal(t, expectedURL, bucketURL)
+	expectedURL := fmt.Sprintf("gs://%s", expectedBucketName)
+	assert.Equal(t, expectedURL, bucketURL)
 
-  gcp.AssertStorageBucketExists(t, expectedBucketName)
+	gcp.AssertStorageBucketExists(t, expectedBucketName)
 
-  instance := gcp.FetchInstance(t, projectId, instanceName)
-  instance.SetLabels(t, map[string]string{"testing": "testing-tag-value2"})
+	instance := gcp.FetchInstance(t, projectId, instanceName)
+	instance.SetLabels(t, map[string]string{"testing": "testing-tag-value2"})
 
-  maxRetries := 12
-  timeBetweenRetries := 5 * time.Second
-  expectedText := "testing-tag-value2"
+	maxRetries := 12
+	timeBetweenRetries := 5 * time.Second
+	expectedText := "testing-tag-value2"
 
-  retry.DoWithRetry(t, fmt.Sprintf("Checking Instance %s for labels", instanceName), maxRetries, timeBetweenRetries, func() (string, error) {
-    instance := gcp.FetchInstance(t, projectId, instanceName)
-    instanceLabels := instance.GetLabels(t)
+	retry.DoWithRetry(t, fmt.Sprintf("Checking Instance %s for labels", instanceName), maxRetries, timeBetweenRetries, func() (string, error) {
+		instance := gcp.FetchInstance(t, projectId, instanceName)
+		instanceLabels := instance.GetLabels(t)
 
-    testingTag, containsTestingTag := instanceLabels["testing"]
-    actualText := strings.TrimSpace(testingTag)
-    if !containsTestingTag {
-      return "", fmt.Errorf("Expected the tag 'testing' to exist")
-    }
+		testingTag, containsTestingTag := instanceLabels["testing"]
+		actualText := strings.TrimSpace(testingTag)
+		if !containsTestingTag {
+			return "", fmt.Errorf("Expected the tag 'testing' to exist")
+		}
 
-    if actualText != expectedText {
-      return "", fmt.Errorf("Expected GetLabelsForComputeInstanceE to return '%s' but got '%s'", expectedTest, actualText)
-    }
+		if actualText != expectedText {
+			return "", fmt.Errorf("Expected GetLabelsForComputeInstanceE to return '%s' but got '%s'", expectedTest, actualText)
+		}
 
-    return "", nil
-  })
+		return "", nil
+	})
 }
-
 
 func TestSshAccessToComputeInstance(t *testing.T) {
 	t.Parallel()
